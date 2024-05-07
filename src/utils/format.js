319252aa73a6a2099553883fdb0f9828eee5c065
Anas Ouh <anasouhtrcg@gmail.com>
2024-05-08 00:46:33 +0200

    Fix time format (months, years)

--- src/utils/format.js
+++ src/utils/format.js
@@ -26,8 +26,14 @@ export function formatDate(date) {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
 
-  if (days > 0) {
+  if (years > 0) {
+    return `${years} an${years > 1 ? "s" : ""}`;
+  } else if (months > 0) {
+    return `${months} mois`;
+  } else if (days > 0) {
     return `${days} jour${days > 1 ? "s" : ""}`;
   } else if (hours > 0) {
     return `${hours} heure${hours > 1 ? "s" : ""}`;
